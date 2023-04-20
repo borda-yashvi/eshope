742d5043f2ec14d58771d3afadde07b811f6a979
borda yashasvi <106402563+borda-yashvi@users.noreply.github.com>
2023-04-20 22:13:52 +0530

    Add files via upload

--- Router/middleware.js
+++ Router/middleware.js
@@ -1,29 +1,14 @@
-const tokenobj = require("../token/token");
-const jwtToken = require("jsonwebtoken");
-
-const midleware = (req, res, next) => {
-    if (tokenobj.token == req.headers.token)
+const tokenObj = require("../Token/token")
+const middleware = (req,res,next)=>
+{
+    if(tokenObj.token == req.headers.token)
     {
-     
-        jwtToken.verify(tokenobj.token, 'skill', function(err, decoded) {
-            if(err){
-                res.json({token:"expire"});
-            }
-            else
-            {
-               
-                next();
-                res.json({token:"valid"});
-            }
-          });
-          
-    
-         
+        next();
     }
-    else {
-        res.json({ msg: "fail in module1.....!" })
+    else
+    {
+        res.json({"msg":"inside middleware fail"})
     }
 }
 
-
-module.exports = midleware;
+module.exports = middleware
